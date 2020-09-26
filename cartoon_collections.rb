be0ad76def62d4bf5d707640bcb96543764fc50a
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 02:13:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,10 @@ end
 end
 
 def list_dwarves(array)
-  
+   array.each do |x|
+puts "Hello #{x}!"
+end
+
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
